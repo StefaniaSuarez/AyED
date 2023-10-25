cba948568700cc9cdfc71fade9a0028687c77b03
Stefania Suarez <ssuarez@frba.utn.edu.ar>
2023-10-25 01:04:18 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,35 +1,25 @@
 #include<iostream>
 #include <iomanip>
+
 using namespace std;
 
-double CPi() {
-
+double CalcularPi() {
     double pi = 0.0;
-    int signo , denom;
-    int max_iteraciones = 1000000; 
-
-    signo=1;
-    denom=1;
-
-
-    for (int a = 0; a < max_iteraciones ; a+-0+) {
-        pi += signo * (1.0 /denom);
-
+    int signo = 1;
+    int denom = 1;
+    
+    while (static_cast<int>(pi*4*1000000) != 3141592)
+    {
+        pi += signo * (1.0/denom);
         denom +=2;
         signo *= -1;
-       
-        if (static_cast<int>(pi* 1e6) == 311592)
-          break;
     }
     
-    return pi * 4;
+    return pi*4;
 }
 
-
 int main() {
-    double pi = CPi();
-    cout << setprecision(7);
-    cout <<"El valor de pi es: "<< pi << endl;
-
+    double pi = CalcularPi();
+    cout <<setprecision(7) <<"El valor es: "<< pi << endl;
     return 0;
 }
